6b24aac23608e2683b724add7a56eda0f343cf52
Sachinpawar86 <106669438+Sachinpawar86@users.noreply.github.com>
2025-01-26 15:06:44 +0530

    Add files via upload

---
+++ mistos.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Local TimeZone
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/India /etc/localtime
+
+# repo init rom
+repo init -u https://github.com/Project-Mist-OS/manifest -b 15 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone -b Mist-15 https://github.com/Sachinpawar86/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync
+/opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
+
+# keys
+# mkdir vendor/lineage-priv
+# cp build-keys/* vendor/lineage-priv
+# echo "============="
+# echo "Keys copied"
+# echo "============="
+
+# Export
+export BUILD_USERNAME=Sachin
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+. build/envsetup.sh
+mistify mojito userdebug
+mist b
+breakfast mojito
