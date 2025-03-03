4c09e4098c1ecd14f3cbe8c81f4dd33e248a3d75
Sachinpawar86 <106669438+Sachinpawar86@users.noreply.github.com>
2025-03-03 11:15:07 +0530

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -32,7 +32,7 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_mojito-ap4a-user
+lunch lineage_mojito-ap4a-userdebug
 echo "============="
 
 # Make cleaninstall
