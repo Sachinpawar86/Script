19bc8ed37b0dc052a2301217c296b1b12ea6f7cc
Sachinpawar86 <106669438+Sachinpawar86@users.noreply.github.com>
2025-06-22 15:47:54 +0530

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -7,19 +7,16 @@ sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/India /etc/localtime
 
 # Rom source repo
-repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Clone local_manifests repository
-git clone -b Evo-15-QPR2 https://github.com/Sachinpawar86/local_manifests .repo/local_manifests
+git clone -b Evo-16 https://github.com/Sachinpawar86/local_manifests .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
-
-# Signed Keys
-crave ssh && git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys && cd vendor/evolution-priv/keys && ./keys.sh && exit
 
 # Sync the repositories
 /opt/crave/resync.sh
@@ -35,7 +32,7 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_mojito-bp1a-user
+lunch lineage_mojito-bp2a-userdebug
 echo "============="
 
 # Make cleaninstall
