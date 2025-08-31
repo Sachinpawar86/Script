fd933586a6fd39df1a8d1517c25431c19f349858
Sachinpawar86 <106669438+Sachinpawar86@users.noreply.github.com>
2025-08-31 09:41:16 +0530

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+# Remove old local_manifests
 rm -rf .repo/local_manifests/
 
 # Local TimeZone
 sudo rm -rf /etc/localtime
-sudo ln -s /usr/share/zoneinfo/Asia/India /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 
-# Rom source repo
+# ROM source repo
 repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
 echo "=================="
 echo "Repo init success"
@@ -19,28 +20,33 @@ echo "Local manifest clone success"
 echo "============================"
 
 # Signed Keys
-crave ssh && git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys && cd vendor/evolution-priv/keys && ./keys.sh && exit
+crave ssh
+git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
+cd vendor/evolution-priv/keys || exit 1
+./keys.sh
+cd ../../..
+echo "===== Keys setup done ====="
 
 # Sync the repositories
 /opt/crave/resync.sh
 echo "============================"
 
 # Export
-export BUILD_USERNAME=Sachin
-export BUILD_HOSTNAME=crave
+export BUILD_USERNAME="Sachin"
+export BUILD_HOSTNAME="crave"
 echo "======= Export Done ======"
 
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
-# Lunch
-lunch lineage_mojito-bp2a-user
+# Lunch (check correct target name, example below)
+lunch evolution_mojito-user
 echo "============="
 
-# Make cleaninstall
+# Make clean install
 make installclean
 echo "============="
 
-# Build rom
+# Build ROM
 m evolution
