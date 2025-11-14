b1ff1ea6761685a61e11497e66c7d98e36017f24
Sachinpawar86 <106669438+Sachinpawar86@users.noreply.github.com>
2025-11-14 21:57:47 +0530

    Add files via upload

---
+++ alphadroid.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Remove Xiaomi device/vendor/kernel trees for onyx
+rm -rf device/xiaomi/onyx
+rm -rf vendor/xiaomi/onyx
+rm -rf device/xiaomi/onyx-kernel
+
+# Remove Xiaomi hardware folder
+rm -rf hardware/xiaomi
+
+# Remove Xiaomi Dolby app
+rm -rf packages/apps/XiaomiDolby
+
+# Remove local manifests
+rm -rf .repo/local_manifests/
+
+# Local TimeZone
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+
+# ROM source repo
+repo init -u https://github.com/alphadroid-project/manifest -b alpha-16.1 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b Alpha-16 https://github.com/Sachinpawar86/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# Export
+export BUILD_USERNAME=Sachin
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch alpha_onyx-userdebug
+echo "============="
+
+# Make clean install
+make installclean
+echo "============="
+
+# Build ROM
+make bacon
+echo "============="
