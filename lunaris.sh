9e7d886e9d74e66f7f3a010034f45ff9e344257d
Sachinpawar86 <sachinbarange86@gmail.com>
2025-12-01 17:11:34 +0530

    Add lunaris.sh script for Xiaomi device setup and build process

--- lunaris.sh
+++ lunaris.sh
@@ -19,24 +19,19 @@ sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 
 # ROM source repo
-repo init -u https://github.com/alphadroid-project/manifest -b alpha-16.1 --git-lfs
+repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Clone local_manifests repository
-git clone -b Alpha-16 https://github.com/Sachinpawar86/local_manifests .repo/local_manifests
+git clone -b Lunaris-16 https://github.com/Sachinpawar86/local_manifests .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
-# Custom Soong
-rm -rf build/soong
-git clone https://github.com/Sachinpawar86/build_soong.git build/soong
-
-# Custom Bionic
-rm -rf bionic
-git clone https://github.com/Sachinpawar86/bionic.git -b alpha-16.1 bionic
+# Camera
+git clone https://gitea.com/goziko32/vendor_gcam.git vendor/mgc
 
 # Sync the repositories
 /opt/crave/resync.sh
@@ -52,7 +47,7 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch alpha_onyx-userdebug
+lunch lineage_onyx-bp2a-user
 echo "============="
 
 # Make clean install
@@ -60,5 +55,5 @@ make installclean
 echo "============="
 
 # Build ROM
-make bacon
+m lunaris
 echo "============="
