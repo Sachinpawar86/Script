e85de5428bb6b9c2d98d5c9d695910cb18062c1b
Sachinpawar86 <106669438+Sachinpawar86@users.noreply.github.com>
2025-12-17 06:37:25 +0530

    Remove camera repository cloning from lunaris.sh
    Removed camera repository cloning from script.

--- lunaris.sh
+++ lunaris.sh
@@ -30,9 +30,6 @@ echo "============================"
 echo "Local manifest clone success"
 echo "============================"
 
-# Camera
-git clone https://gitea.com/goziko32/vendor_gcam.git vendor/mgc
-
 # Sync the repositories
 /opt/crave/resync.sh
 echo "============================"
