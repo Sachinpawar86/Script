46f446bed5e71250f43a17ea8d2322536875a32a
Sachinpawar86 <106669438+Sachinpawar86@users.noreply.github.com>
2026-01-02 20:12:38 +0530

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -19,7 +19,7 @@ sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 
 # ROM source repo
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16-QPR0 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
