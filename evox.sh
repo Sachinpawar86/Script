dc28fce38fdc084d43cebc9244fc04fca6dfbdae
Sachinpawar86 <106669438+Sachinpawar86@users.noreply.github.com>
2026-02-06 08:50:48 +0530

    Change lunch target from user to eng variant

--- evox.sh
+++ evox.sh
@@ -37,7 +37,7 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_mojito-bp3a-user
+lunch lineage_mojito-bp3a-eng
 echo "============="
 
 # Make clean install
