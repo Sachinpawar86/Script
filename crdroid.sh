36a5821b11199583ca048d45c0f5724a804e22a6
Sachin_07 <106669438+Sachinpawar86@users.noreply.github.com>
2026-03-09 21:37:37 +0530

    Add files via upload

---
+++ crdroid.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Remove old local_manifests
+rm -rf .repo/local_manifests/
+
+# Local TimeZone
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+
+# ROM source repo
+repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b crDroid https://github.com/Sachinpawar86/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# Export
+export BUILD_USERNAME=Sachin
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Make clean install
+make installclean
+echo "============="
+
+# Build ROM
+brunch mojito
