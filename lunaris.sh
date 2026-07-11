314556e0ddd32d1309efe5678356223b0529a6d8
Sachin_07 <106669438+Sachinpawar86@users.noreply.github.com>
2026-07-11 10:28:51 +0530

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -43,10 +43,6 @@ echo "====== Envsetup Done ======="
 lunch lineage_onyx-bp4a-user
 echo "============="
 
-# Make clean install
-make installclean
-echo "============="
-
 # Build ROM
 m bacon
 echo "============="
