02593ba1cb583939a53334dee4f78219551f46f2
Sachin_07 <106669438+Sachinpawar86@users.noreply.github.com>
2026-08-14 07:00:39 +0530

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -42,7 +42,7 @@ curl -L https://github.com/sp-projectss/bionic/commit/8c732ec79000384e52de0efa65
 cd ..
 
 # Export
-export WITH_GMS=false
+export WITH_GMS=true
 echo "======= Export Done ======"
 
 # Set up build environment
