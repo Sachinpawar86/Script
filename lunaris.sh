da02f109e592c12dcc1edf339ec4038a732386f8
Sachin_07 <106669438+Sachinpawar86@users.noreply.github.com>
2026-08-27 07:36:41 +0530

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -30,7 +30,7 @@ echo "============================"
 echo "============================"
 
 # Export
-export WITH_GMS=true
+export WITH_GMS=false
 echo "======= Export Done ======"
 
 # Set up build environment
