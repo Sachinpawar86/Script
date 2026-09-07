d008249e6bce804e03255bd080b5fda47a04b744
Sachin_07 <106669438+Sachinpawar86@users.noreply.github.com>
2026-09-07 15:45:50 +0530

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -39,6 +39,7 @@ echo "============================"
 # Bionic
 cd bionic
 curl -L https://github.com/Sachinpawar86/bionic/commit/af180de003807047d69b7c10a83ff05891b71ed3.patch | git am
+cd ..
 
 # Export
 export WITH_GMS=true
