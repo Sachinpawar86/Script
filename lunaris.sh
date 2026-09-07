d4c8affb2f340c7743bc3f11c9abe99753ab8a4f
Sachin_07 <106669438+Sachinpawar86@users.noreply.github.com>
2026-09-07 08:13:08 +0530

    Update lunaris.sh

--- lunaris.sh
+++ lunaris.sh
@@ -36,6 +36,10 @@ echo "============================"
 /opt/crave/resync.sh
 echo "============================"
 
+# Bionic
+cd bionic
+curl -L https://github.com/Sachinpawar86/bionic/commit/af180de003807047d69b7c10a83ff05891b71ed3.patch | git am
+
 # Export
 export WITH_GMS=true
 echo "======= Export Done ======"
